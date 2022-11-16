cf841da362e9365cc292657b85a9cea29264f099
heejeong <hjx66@naver.com>
2022-11-16 15:29:32 +0900

    test: add() 새글 등록 테스트

--- src/test/java/com/mustache/bbs/controller/ArticleRestControllerTest.java
+++ src/test/java/com/mustache/bbs/controller/ArticleRestControllerTest.java
@@ -1,5 +1,8 @@
 package com.mustache.bbs.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mustache.bbs.domain.dto.ArticleAddRequest;
+import com.mustache.bbs.domain.dto.ArticleAddResponse;
 import com.mustache.bbs.domain.dto.ArticleDto;
 import com.mustache.bbs.service.ArticleService;
 import org.junit.jupiter.api.DisplayName;
@@ -7,10 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -23,6 +29,30 @@ class ArticleRestControllerTest {
 
     @MockBean
     ArticleService articleService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("새 글 등록이 잘 되는지 테스트")
+    void add() throws Exception{
+        ArticleAddRequest dto = new ArticleAddRequest("제목목", "내요용");
+        given(articleService.add(any()))
+                .willReturn(new ArticleAddResponse(1L, dto.getTitle(), dto.getContent()));
+
+        mockMvc.perform(post("/api/v1/articles")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(dto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.title").exists())
+                .andExpect(jsonPath("$.title").value("제목목"))
+                .andExpect(jsonPath("$.content").exists())
+                .andExpect(jsonPath("$.content").value("내요옹"))
+                .andDo(print());
+
+
+    }
 
     @Test
     @DisplayName("1개의 게시글이 Json형태로 Response가 잘 오는 test")
@@ -45,7 +75,6 @@ class ArticleRestControllerTest {
                 .andDo(print());
 
         verify(articleService).getArticle(articleId);
-
     }
 
 }
